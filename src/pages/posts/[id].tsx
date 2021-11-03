5f0e59c47c8936d7ba1d504db7093e098f3fd216
Airthee <13355624+Airthee@users.noreply.github.com>
2021-11-03 07:32:47 +0000

    Fix pre tag overflow

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Layout from '../../components/layout'
-import Date from '../../components/date'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import { PostType } from '../../lib/posts'
-import utilStyles from '../../styles/utils.module.css'
-import React, { FunctionComponent } from 'react'
+import Head from "next/head";
+import Layout from "../../components/layout";
+import Date from "../../components/date";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import { PostType } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
+import React, { FunctionComponent } from "react";
 import {
   FacebookShareButton,
   FacebookIcon,
@@ -13,27 +13,35 @@ import {
   LinkedinShareButton,
   LinkedinIcon,
   EmailShareButton,
-  EmailIcon
-} from 'react-share'
+  EmailIcon,
+} from "react-share";
 
 type PostProps = {
-  postData: PostType
-}
+  postData: PostType;
+};
 
 const Post: FunctionComponent<PostProps> = ({ postData }) => {
-  const postUrl = `https://blog.airthee.com/posts/${postData.id}`
+  const postUrl = `https://blog.airthee.com/posts/${postData.id}`;
   const shareIconProps = {
     size: 35,
     round: true,
-  }
+  };
   const shareSection = (
     <div className={utilStyles.alignCenter}>
-      <FacebookShareButton url={postUrl}><FacebookIcon {...shareIconProps}/></FacebookShareButton>
-      <TwitterShareButton url={postUrl}><TwitterIcon {...shareIconProps}/></TwitterShareButton>
-      <LinkedinShareButton url={postUrl}><LinkedinIcon {...shareIconProps}/></LinkedinShareButton>
-      <EmailShareButton url={postUrl}><EmailIcon {...shareIconProps}/></EmailShareButton>
+      <FacebookShareButton url={postUrl} className={utilStyles.mx10}>
+        <FacebookIcon {...shareIconProps} />
+      </FacebookShareButton>
+      <TwitterShareButton url={postUrl} className={utilStyles.mx10}>
+        <TwitterIcon {...shareIconProps} />
+      </TwitterShareButton>
+      <LinkedinShareButton url={postUrl} className={utilStyles.mx10}>
+        <LinkedinIcon {...shareIconProps} />
+      </LinkedinShareButton>
+      <EmailShareButton url={postUrl} className={utilStyles.mx10}>
+        <EmailIcon {...shareIconProps} />
+      </EmailShareButton>
     </div>
-  )
+  );
 
   return (
     <Layout>
@@ -41,8 +49,11 @@ const Post: FunctionComponent<PostProps> = ({ postData }) => {
         <title>{postData.title}</title>
         <meta property="og:title" content={postData.title} />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content={`https://blog.airthee.com/posts/${postData.id}`} />
-        <meta property="og:locale" content={postData.lang ?? 'en_US'} />
+        <meta
+          property="og:url"
+          content={`https://blog.airthee.com/posts/${postData.id}`}
+        />
+        <meta property="og:locale" content={postData.lang ?? "en_US"} />
       </Head>
 
       <article>
@@ -55,23 +66,23 @@ const Post: FunctionComponent<PostProps> = ({ postData }) => {
         {shareSection}
       </article>
     </Layout>
-  )
-}
-export default Post
+  );
+};
+export default Post;
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds().map(id => ({params: { id }}))
+  const paths = getAllPostIds().map((id) => ({ params: { id } }));
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
